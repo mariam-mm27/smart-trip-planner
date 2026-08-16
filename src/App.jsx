4d93803786a492ba791eef1afd1398a75f378fd1
Barthina-Reda <barthinareda8@gmail.com>
2026-08-16 18:22:09 +0300

    PLACE

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
+import Place from "./components/features/PlaceCard/Place";
 import PlaceCard from "./components/features/PlaceCard/PlaceCard";
 
 function App() {
-  return (
-    <PlaceCard
-      title="Example Place"
-      description="This is an example place."
-      location="Example Location"
-      stars={4.5}
-      reviews={100}
-      imgTitle="Example Image"
-    />
-  );
+  return <>
+  <Place />
+  
+  </>
 }
 
 export default App;
--- src/components/features/PlaceCard/Place.jsx
+++ src/components/features/PlaceCard/Place.jsx
@@ -2,13 +2,29 @@ import React from 'react'
 import PlaceCard from './PlaceCard.jsx'
 
 export default function Place() {
-    title = "Place"
-    location = "Location"
-    stars = 4.5
-    imgTitle = "Image Title"
-    image = "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+    const title = "Place"
+    const location = "Location"
+    const stars = 4.5
+    const imgTitle = "Image Title"
+    const image = "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
   return <>
-  <PlaceCard />
+  <PlaceCard
+      title="Example Place"
+      description="This is an example place."
+      location="Example Location"
+      stars={4.5}
+      reviews={100}
+      imgTitle="Example Image"
+    />
+
+    <PlaceCard
+      title="Example Place"
+      description="This is an example place."
+      location="Example Location"
+      stars={4.5}
+      reviews={100}
+      imgTitle="Example Image"
+    />
   
   </>
 }
