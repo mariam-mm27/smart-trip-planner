a6c2e770d01fa9321f07580a64ed0aff791423d5
Mariam Khaled <mariam22khalid22@gmail.com>
2026-08-18 00:47:42 +0300

    feat(auth): complete authentication flow with Supabase and Google OAuth and forget password

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,23 @@ import { useState } from "react";
 import Place from "./components/features/PlaceCard/Place";
 import PlaceCard from "./components/features/PlaceCard/PlaceCard";
 import Favorites from "./pages/Favorites/Favorites";
+import { AuthProvider } from './context/AuthContext';
+import { AuthForm } from './components/features/Auth/AuthForm';
+import { ResetPassword } from './components/features/Auth/ResetPassword';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 
 function App() {
-
+const isResetPasswordPath = window.location.pathname.startsWith('/reset-password');
+  return (
+    <AuthProvider>
+<div className="auth-wrapper">
+  {isResetPasswordPath ? <ResetPassword /> : <AuthForm />}
+      </div>
+    </AuthProvider>
+  );
   const [favorites, setFavorites] = useState([]);
-  
   return <>
   <BrowserRouter>
     <Link to="/favorites">Favorites</Link>
@@ -38,6 +48,8 @@ function App() {
   </BrowserRouter>
   
   </>
+
+  
 }
 
 export default App;
